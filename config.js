072be1521448a150026165298019732f8bb00cf9
José Luis Zenteno De León <jldeep@icloud.com>
2016-12-28 20:08:28 -0600

    Add Moongose support

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
 'use strict'
 
 module.exports = {
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  mongo: {
+    port: process.env.MONGO_PORT || 27017,
+    host: process.env.MONGO_HOST || 'localhost',
+    db: process.env.MONGO_DATABASE || 'koa-api',
+    user: process.env.MONGO_USER || '',
+    password: process.env.MONGO_PASSWORD || ''
+  }
 }
