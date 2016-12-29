3ff1a349409c8ff104c77614570487de2bbc3946
José Luis Zenteno De León <jldeep@icloud.com>
2016-12-28 21:00:15 -0600

    Read router folder and add to Koa

--- server.js
+++ server.js
@@ -4,12 +4,23 @@ const koa = require('koa')
 const server = koa()
 const logger = require('koa-logger')
 const config = require('./config')
+const fs = require('fs')
+const path = require('path')
 
 // logger
 server.use(logger())
 
-// routers
-server.use(require('./routers/api-router.js').routes())
+// Read routers folder and add it
+for (let controller of fs.readdirSync(path.resolve('routers'))) {
+  let file = path.resolve('routers', controller)
+  if (/-router.js$/.test(file) && fs.statSync(file).isFile()) {
+    try {
+      server.use(require(file).routes())
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
 
 if (module.parent) {
   module.exports = server.listen()
